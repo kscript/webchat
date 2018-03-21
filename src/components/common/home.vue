b50687a308ef79a9b2f4e4dee1fb91162beaf052
kscript <2046@live.cn>
2018-03-21 17:37:54 +0800

    基于Vuejs element-ui开发一个web聊天工具

---
+++ src/components/common/home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+    <v-header></v-header>
+    <template v-if="$route.meta.keep">
+      <keep-alive></keep-alive>
+    </template>
+    <template v-else>
+      <router-view></router-view>
+    </template>
+    <v-footer></v-footer>
+  </div>
+</template>
+<script>
+import header from './header.vue'
+import footer from './footer.vue'
+export default {
+  data () {
+    return {
+    }
+  },
+  components: {
+    'v-header': header,
+    'v-footer': footer
+  }
+}
+</script>
