49d242fcd111a578cfa8a052154b473df88eafe7
kscript <1258@live.cn>
2018-03-22 14:10:20 +0800

    添加axios

---
+++ src/axios/index.js
@@ -0,0 +1,30 @@
+import axios from 'axios'
+import qs from 'qs'
+
+let $axios = axios.create({
+  baseURL: '/',
+  timeout: 15000,
+  transformRequest: [function (data) {
+    if (data instanceof FormData) {
+      return data
+    } else {
+      return qs.stringify(data)
+    }
+  }]
+})
+
+// 添加请求拦截器
+$axios.interceptors.request.use(function (config) {
+  return config
+}, function (error) {
+  return Promise.reject(error)
+})
+
+// 添加响应拦截器
+$axios.interceptors.response.use(function (response) {
+  return Promise.resolve(response)
+}, function (error) {
+  return Promise.reject(error)
+})
+
+export default $axios
